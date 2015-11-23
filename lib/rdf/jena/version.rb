f46f0d015dbc888b120392ca91d756bd11f87f44
Anthony Bargnesi <abargnesi@gmail.com>
2015-11-22 22:35:01 -0500

    change to define the RDF module in version.rb
    RDF module was not defined yet (i.e. calling RDF::Jena)
    
    this caused rdf-jena.gemspec to return an error when called

--- lib/rdf/jena/version.rb
+++ lib/rdf/jena/version.rb
@@ -1,24 +1,26 @@
-module RDF::Jena
+module RDF
+  module Jena
 
-  module Version
+    module Version
 
-    VERSION_FILE = File.join(File.expand_path(File.dirname(__FILE__)), '..', '..', '..', 'VERSION')
-    MAJOR, MINOR, TINY, EXTRA = File.read(VERSION_FILE).chomp.split('.')
-    STRING = [MAJOR, MINOR, TINY, EXTRA].compact.join('.')
+      VERSION_FILE = File.join(File.expand_path(File.dirname(__FILE__)), '..', '..', '..', 'VERSION')
+      MAJOR, MINOR, TINY, EXTRA = File.read(VERSION_FILE).chomp.split('.')
+      STRING = [MAJOR, MINOR, TINY, EXTRA].compact.join('.')
 
-    # Returns version string.
-    #
-    # @return [String]
-    def self.to_s() STRING end
+      # Returns version string.
+      #
+      # @return [String]
+      def self.to_s() STRING end
 
-    # Returns version string.
-    #
-    # @return [String]
-    def self.to_str() STRING end
+      # Returns version string.
+      #
+      # @return [String]
+      def self.to_str() STRING end
 
-    # Returns version {Array}.
-    #
-    # @return [Array(Integer, Integer, Integer)]
-    def self.to_a() [MAJOR, MINOR, TINY] end
+      # Returns version {Array}.
+      #
+      # @return [Array(Integer, Integer, Integer)]
+      def self.to_a() [MAJOR, MINOR, TINY] end
+    end
   end
 end
